e835cd696c92f7d7a4e67afb2cb7d8f7e94c2787
SaiProton <shayanr1001@gmail.com>
2022-11-17 11:32:48 -0500

    Chapter 4 complete

---
+++ src/ch4/mod.rs
@@ -0,0 +1,106 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug)]
+struct CubeSat {
+    id: u64,
+}
+
+#[derive(Debug)]
+struct Mailbox {
+    messages: Vec<Message>,
+}
+
+#[derive(Debug)]
+struct Message {
+    to: u64,
+    content: String,
+}
+
+#[derive(Debug)]
+struct GroundStation {
+    radio_freq: f64,
+}
+
+impl CubeSat {
+    fn recv(&self, mailbox: &mut Mailbox) -> Option<Message> {
+        mailbox.deliver(self)
+    }
+}
+
+impl GroundStation {
+    fn connect(sat_id: u64) -> CubeSat {
+        CubeSat { id: sat_id }
+    }
+
+    fn send(mailbox: &mut Mailbox, msg: Message) {
+        mailbox.post(msg);
+    }
+}
+
+impl Mailbox {
+    fn post(&mut self, msg: Message) {
+        self.messages.push(msg);
+    }
+
+    fn deliver(&mut self, recipient: &CubeSat) -> Option<Message> {
+        for i in 0..self.messages.len() {
+            if self.messages[i].to == recipient.id {
+                let msg = self.messages.remove(i);
+                return Some(msg);
+            }
+        }
+
+        None
+    }
+}
+
+fn fetch_sat_ids() -> Vec<u64> {
+    vec![1, 2, 3]
+}
+
+pub fn main() {
+    let mut mail = Mailbox { messages: vec![] };
+
+    let base = GroundStation { radio_freq: 1.11 };
+
+    let sat_ids = fetch_sat_ids();
+
+    for sat_id in sat_ids {
+        let sat = GroundStation::connect(sat_id);
+        let msg = Message {
+            to: sat_id,
+            content: String::from("hello"),
+        };
+        GroundStation::send(&mut mail, msg);
+    }
+
+    let sat_ids = fetch_sat_ids();
+
+    for sat_id in sat_ids {
+        let sat = GroundStation::connect(sat_id);
+
+        let msg = sat.recv(&mut mail);
+        println!("{:?}: {:?}", sat, msg);
+    }
+
+    // Allows for the creation of multiple mutable references.
+    let base: Rc<RefCell<GroundStation>> =
+        Rc::new(RefCell::new(GroundStation { radio_freq: 87.65 }));
+
+    println!("base: {:?}", base);
+
+    {
+        let mut base_2 = base.borrow_mut();
+        base_2.radio_freq -= 12.34;
+        println!("base_2 {:?}", base_2);
+    }
+
+    println!("base: {:?}", base);
+
+    let mut base_3 = base.borrow_mut();
+    base_3.radio_freq += 43.21;
+
+    println!("base: {:?}", base);
+    println!("base_3: {:?}", base_3);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod ch1;
 mod ch2;
 mod ch3;
+mod ch4;
 
 fn main() {
     // ch1::main();
     // ch2::main();
-    ch3::main();
+    // ch3::main();
+    ch4::main();
 }
